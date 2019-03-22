ceed5c955f963c1859fd5ec309c401eb8a88f2c7
Kidd75201 <jbezal@gmail.com>
2019-03-22 12:31:26 -0500

    Initial project setup, created and connected to MongoDB

---
+++ project-1_MERN_Shopping_List/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://jbeza:jbeza123@mernshopping-uzh49.mongodb.net/test?retryWrites=true"
+};
---
+++ project-1_MERN_Shopping_List/server.js
@@ -0,0 +1,24 @@
+// Bring in dependencies
+const express = require("express");
+const mongoose = require("mongoose"); // ORM interact with MongoDB
+const bodyParser = require("body-parser"); // takes request and get data from body
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+// create var to port
+const port = process.env.PORT || 5000;
+
+// listen to port
+app.listen(port, () => console.log(`Server started on port ${port}`));
