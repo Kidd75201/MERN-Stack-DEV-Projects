3f3a8dc7c5ef08ac4586bd3440d4ea29a3a1bf6d
Kidd75201 <jbezal@gmail.com>
2019-03-24 02:57:58 -0500

    Adding heroku feature for deployment

--- project-1_MERN_Shopping_List/server.js
+++ project-1_MERN_Shopping_List/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose"); // ORM interact with MongoDB
 const bodyParser = require("body-parser"); // takes request and get data from body
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -21,6 +22,16 @@ mongoose
 
 // Use Routes
 app.use("/api/items", items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // create var to port
 const port = process.env.PORT || 5000;
